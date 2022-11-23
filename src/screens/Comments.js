4c7b75da3bb3f567918de9379f3382795f28db9d
franciscojofre <jjofrelopez@udesa.edu.ar>
2022-11-23 17:14:58 -0300

    estilos omentarios y orden de comentarios en comments y post

--- src/screens/Comments.js
+++ src/screens/Comments.js
@@ -53,8 +53,8 @@ class Comments extends Component {
       <View style={styles.container}>
         <View style={styles.box}>
 
-            <TouchableOpacity stlye={styles.arrow} onPress={() => this.volver()}>
-              <AntDesign name="arrowleft" size={24} color="#5c0931" />
+            <TouchableOpacity onPress={() => this.volver()}>
+              <AntDesign name="arrowleft" size={36} color="#5c0931" />
             </TouchableOpacity>
 
           {this.state.commentCount == 0 
@@ -65,20 +65,29 @@ class Comments extends Component {
               </Text>
 
               <FlatList
-                    data={this.state.comentarios}
+                    data={this.state.comentarios.sort((a, b) => b.createdAt - a.createdAt)}
                     keyExtractor={( item ) => item.createdAt.toString()}
-                    renderItem={({item}) => <Text>{item.owner}: {item.description}</Text>} /> 
+                    renderItem={({item}) => 
+                                <>
+                                    <View style={styles.comentarios}>
+                                        <TouchableOpacity style={styles.comentarios.boxOwner} onPress={() => this.props.navigation.navigate('Go Back', {user: item.owner})}>
+                                            <Text style={styles.comentarios.boxOwner.owner}>{item.owner}: </Text>
+                                        </TouchableOpacity> 
+                                        <Text style={styles.comentarios.texto}>{item.description}</Text>
+                                    </View>
+                                </>                  
+                            }/> 
             </>
           }
           <View style={styles.containerComm}>
               <TextInput
-                style={styles.form}
+                style={styles.commentsInput}
                 keyboardType='default'
                 placeholder='Your comment!'
                 onChangeText={ text => this.setState({comment:text}) }
                 value={this.state.comment} />
               <TouchableOpacity onPress={() => this.comment(this.state.comment)}>
-                <Text style={styles.textButton}>
+                <Text style={styles.boton}>
                   Up Load
                 </Text>
               </TouchableOpacity>
@@ -94,20 +103,22 @@ class Comments extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
+    marginVertical: 20
   },
   box:{
     textAlign: 'center',
     border: '1px solid #5c0931',
-    width: '80%',
+    width: 400,
+    flex: 1,
     backgroundColor: 'white'
   },
   form:{
-    width: '90%',
+    width: 350,
     border: '1px solid #5c0931',
   },
   text:{
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     marginVertical: 10
   },
@@ -126,11 +137,45 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginVertical: 5,
     marginLeft: 8
-  }
+  },
+  cajacomentarios: {
+    alignItems: 'flex-start',
+},
+comentarios: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'flex-start',
+    padding: 5,
+    boxOwner: {
+        width: 'auto',
+        textAlign: 'start',
+        owner: {
+            fontWeight: 550,
+            fontSize: 20
+        }
+    },
+    texto: {
+        width: 'auto',
+        textAlign: 'start',
+        fontSize: 20
+    }
+},
+commentsInput: {
+  backgroundColor: '#CCCCCC',
+  borderWidth: 1,
+  borderColor: '#CCCCCC',
+  paddingLeft: 5,
+  marginRight: 10,
+  shadowOpacity: 20
+},
+boton: {
+  backgroundColor: '#5c0931',
+  color: 'white',
+  borderRadius: 3,
+  padding: 3,
+  alignSelf: 'center',
+  fontWeight: 'bold'
+}
 })
 
 export default Comments
-
-//TAREAS
-// 1. ORDENAR LOS COMENTARIOS
-// 2. ESTILOS
